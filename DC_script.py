4792950bad9e047a68e4fe7840e84eb76e0018ea
Peter Teuben <teuben@gmail.com>
2022-11-15 22:13:35 -0500

    clarification

--- DC_script.py
+++ DC_script.py
@@ -8,14 +8,14 @@ Based on the work at the Workshop
 Lorentz Center, Leiden, August 2019, 
 and subsequent follow-up work. 
 
-Run under CASA 6. (CASA5 no longer supported)
+Run under CASA 6. (CASA 5 no longer supported)
 
 Typical use in CASA6:
      execfile("DC_script.py")
 
 """
 
-#  no not modify this file, instead copy a template from DC_pars_*.py to DC_pars.py and edit that
+#  no not modify DC_pars.py, instead copy (or symlink) a template from DC_pars_*.py to DC_pars.py and edit that
 
 execfile("DC_pars.py", globals())          # user modifiable parameters (templates exist)
 
